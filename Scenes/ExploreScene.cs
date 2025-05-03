d7a49e4f0918802f58898c551d258f20c6af6cd1
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-05-02 14:36:11 -1000

    Add InputManager to ExploreScene
    I need to work out some of the bugs. I'll probably need to refactor the commands. It shouldn't be too hard, it'll be a lot of adding the InputManager and MenuOption to them.

--- Scenes/ExploreScene.cs
+++ Scenes/ExploreScene.cs
@@ -4,13 +4,29 @@
 
     public override void Run()
     {
-        Console.Write("> ");
+        var validCommands = CommandManager.Commands
+            .Where(command => command.IsValid(player, player.CurrentLocation))
+            .OrderBy(cmd => cmd.Name)
+            .ToList();
+
+        List<MenuOption> options = new List<MenuOption>();
+
+        foreach (var command in validCommands)
+        {
+            string[] temp = ["temp"];
+            MenuOption opt = new MenuOption(command.Name, () => command.Execute(player, player.CurrentLocation, temp));
+            options.Add(opt);
+        }
+
+        InputManager.RunMenu(options);
+
+/*        Console.Write("> ");
         String? input = Console.ReadLine();
 
         if (input != null)
         {
             Parser.Parse(player, player.CurrentLocation, input);
-        }
+        }*/
     }
 
     public override void Enter()
