9562161312bcabfc96b7cf73e9be7342af186ee9
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-18 16:59:34 -1000

    Add new Character Class
    I've added the initial new character class. One thing to note, the new character class doesn't track location, this makes more sense, let the Location deal with who's there.

--- Characters/Character.cs
+++ Characters/Character.cs
@@ -1,3 +1,21 @@
-﻿public class Character
+﻿public abstract class Character
 {
+    public string Name { get; protected set; }
+    public int HP { get; protected set; }
+    public bool IsAlive { get; protected set; } = true;
+    public int Strength { get; protected set; }
+    public int Dexterity { get; protected set; }
+    public int Wisdom { get; protected set; }
+    public int ArmorValue { get; protected set; }
+    //public Inventory Inventory { get; protected set; } // TODO implement Inventory class
+
+    public Character()
+    {
+        this.Name = string.Empty;
+        this.HP = 0;
+        this.Strength = Dice.D(6, 3);
+        this.Dexterity = Dice.D(6, 3);
+        this.Wisdom = Dice.D(6, 3);
+        this.ArmorValue = 0;
+    }
 }
---
+++ Map/Location.cs
@@ -0,0 +1,8 @@
+﻿public class Location
+{
+    public string Name { get; protected set; }
+    public List<Location> Exits = new List<Location>();
+    private List<Character> Characters = new List<Character>();
+    //private Invetory Inventory; TODO Implement Inventory
+
+}
