d8aa565988de2f84e973ba54b90860626d9f1d73
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-20 14:05:29 -1000

    Chore: Pushing to switch computers
    I'm switching to my home machine, so I will push now to continue working on that computer. This is an in-progress update beginning to implement the LocationBuilder feature.

---
+++ Map/LocationBuilder.cs
@@ -0,0 +1,38 @@
+﻿public class LocationBuilder
+{
+    private Location _location = new Location();
+
+    public LocationBuilder()
+    {
+        this.Reset();
+    }
+
+    public void Reset()
+    {
+        this._location = new Location();
+    }
+
+    public LocationBuilder AddTag(string tag) 
+    {
+        this._location.AddTag(tag);
+        return this;
+    }
+    public LocationBuilder AddExit(Guid id) 
+    {
+        this._location.AddExit(id);
+        return this;
+    }
+
+    public LocationBuilder WithName(string name)
+    {
+        // TODO
+        return this; 
+    }
+
+    public Location Build()
+    {
+        Location res = this._location;
+        this.Reset();
+        return res;
+    }
+}
