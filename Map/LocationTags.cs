efef2240120eb78d21b9273e0afa6e06e5eaf03a
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-21 18:46:47 -1000

    Feat: Add StaticWorldGenerator
    I've got the preliminary, working StaticWorldGenerator. This will generate a simple world for testing purposes. Next step, add characters to the world

--- Map/LocationTags.cs
+++ Map/LocationTags.cs
@@ -3,4 +3,6 @@
     public const string StartingLocation = "Starting Location";
     public const string Path = "Path";
     public const string SafeZone = "Safe Zone";
+    public const string Building = "Building";
+    public const string Nature = "Nature";
 }
--- Map/StaticWorldGenerator.cs
+++ Map/StaticWorldGenerator.cs
@@ -1,11 +1,67 @@
 ﻿
 public class StaticWorldGenerator : IWorldGenerator
 {
+    private record LocationBlueprint(string Key, string Name, string[] Tags);
+    private record ConnectionBlueprint(string From, string To);
+
+    List<LocationBlueprint> locationBlueprints = new List<LocationBlueprint>
+    {
+        new("Campfire", "Cozy Campfire", new[] { LocationTags.StartingLocation, LocationTags.SafeZone} ),
+        new("Path", "Worn Path", new[] { LocationTags.Path }),
+        new("Shack", "Hermit's Shack", new[] { LocationTags.Building }),
+        new("Stream", "Babbling Brook", new[] { LocationTags.Nature }),
+        new("Clearing", "Gleaming Glade", new[] { LocationTags.Nature })
+    };
+
+    List<ConnectionBlueprint> connectionBlueprints = new List<ConnectionBlueprint>
+    {
+        new("Campfire", "Path"),
+        new("Path", "Shack"),
+        new("Path", "Stream"),
+        new("Path", "Clearing")
+    };
+
     public WorldGenerationResult GenerateWorld()
     {
-        Dictionary<Guid, Location> theMap = new Dictionary<Guid, Location>();
+        var theMap = new Dictionary<Guid, Location>();
+        var keyToGuideMap = new Dictionary<string, Guid>();
         Guid startingGuid = Guid.Empty;
-        // TODO Add several locations for MVP build
+        LocationBuilder lb = new LocationBuilder();
+        
+        foreach (var blueprint in locationBlueprints)
+        {
+            lb.WithName(blueprint.Name);
+            foreach (var tag in blueprint.Tags)
+            {
+                lb.AddTag(tag);
+            }
+
+            Location newLoc = lb.Build();
+
+            theMap.Add(newLoc.Id, newLoc);
+            keyToGuideMap.Add(blueprint.Key, newLoc.Id);
+        }
+
+        foreach (var connection in connectionBlueprints)
+        {
+            Guid fromId = keyToGuideMap[connection.From];
+            Guid toId = keyToGuideMap[connection.To];
+
+            Location fromLoc = theMap[fromId];
+            Location toLoc = theMap[toId];
+
+            fromLoc.AddExit(toId);
+            toLoc.AddExit(fromId);
+        }
+
+        startingGuid = keyToGuideMap["Campfire"];
+
+        Console.WriteLine("World Build Complete");
+        foreach (var (_, value) in theMap)
+        {
+            Console.WriteLine(value.Name);
+        }
+
         return new WorldGenerationResult(theMap, startingGuid);
     }
 }
