5bf7447754d9335b8ad161b4bcd72f6d66637569
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-21 21:30:14 -1000

    Test: Add Character Tests and Initial MapManager Test
    I've got two test files I'm updating here. One with the Character tests and one with the map manager tests. I need to expand these and add additional tests, but this works for now.

--- console_rpg_app.Tests/CharacterTests.cs
+++ console_rpg_app.Tests/CharacterTests.cs
@@ -33,5 +33,37 @@ namespace console_rpg_app.Tests
             enemy.TakeDamage(damageAmount);
             Assert.Equal(expectedIsAlive, enemy.IsAlive);
         }
+
+        [Fact]
+        public void Heal_WhenCalled_AddsCorrectAmountOfHealth()
+        {
+            var builder = new CharacterBuilder()
+                .WithMaxHP(100)
+                .WithCurrentHP(25);
+            var factory = new CharacterFactory();
+            var player = factory.CreateCharacter(builder, CharacterType.Player);
+
+            int healAmount = 25;
+            int expectedHP = 50;
+
+            player.Heal(healAmount);
+            Assert.Equal(expectedHP, player.CurrentHP);
+        }
+
+        [Fact]
+        public void Heal_WhenCalled_HealthDoesNotExceedMaxHP()
+        {
+            var builder = new CharacterBuilder()
+                .WithMaxHP(100)
+                .WithCurrentHP(80);
+            var factory = new CharacterFactory();
+            var player = factory.CreateCharacter(builder, CharacterType.Player);
+
+            int healAmount = 25;
+            int expectedHP = 100;
+
+            player.Heal(healAmount);
+            Assert.Equal(expectedHP, player.CurrentHP);
+        }
     }
 }
---
+++ console_rpg_app.Tests/MapManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using NSubstitute;
+
+namespace console_rpg_app.Tests
+{
+    public class MapManagerTests
+    {
+        [Fact]
+        public void Constructor_WhenGivenAWorldGenerator_SetsLocationsAndStartingID()
+        {
+            var fakeStartingId = Guid.NewGuid();
+            var fakeLocation = new Location("Fake Town", id: fakeStartingId);
+            var fakeMap = new Dictionary<Guid, Location> { {  fakeStartingId, fakeLocation } };
+            var fakeResult = new WorldGenerationResult(fakeMap, fakeStartingId);
+
+            var mockGenerator = Substitute.For<IWorldGenerator>();
+
+            mockGenerator.GenerateWorld().Returns(fakeResult);
+
+            var mapManager = new MapManager(mockGenerator);
+
+            Assert.Single(mapManager.Locations);
+            Assert.Equal(fakeStartingId, mapManager.CurrentLocation);
+            Assert.Equal("Fake Town", mapManager.Locations[fakeStartingId].Name);
+        }
+    }
+}
