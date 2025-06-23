e086c8b1cd77a03fed32f535f80cc197e6763f07
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-06-23 11:14:15 -1000

    Feat: Add basic Menu
    The basic menu is added, this should loop through the menu items and allow the player to select one to execute

---
+++ console_rpg_app/Menu/ConsoleRenderer.cs
@@ -0,0 +1,31 @@
+﻿public class ConsoleRenderer
+{
+    public void Render(string headerText, Menu menu)
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine(headerText);
+
+        int menuTopPosition = Console.CursorTop;
+
+        for (int i = 0; i < menu.Actions.Count; i++)
+        {
+            Console.SetCursorPosition(0, menuTopPosition + i);
+            bool isSelected = (i == menu.SelectedIndex);
+
+            if (isSelected)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine($"> {menu.Actions[i].Description}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"  {menu.Actions[i].Description}");
+            }
+        }
+    }
+}
---
+++ console_rpg_app/Menu/Menu.cs
@@ -0,0 +1,25 @@
+﻿public class Menu
+{
+    public IReadOnlyList<IGameAction> Actions { get; }
+    public int SelectedIndex { get; private set; }
+
+    public Menu(List<IGameAction> actions)
+    {
+        Actions = actions;
+    }
+
+    public void MoveUp()
+    {
+        SelectedIndex = (SelectedIndex == 0) ? Actions.Count - 1 : SelectedIndex - 1;
+    }
+
+    public void MoveDown()
+    {
+        SelectedIndex = (SelectedIndex + 1) % Actions.Count;
+    }
+
+    public void Select()
+    {
+        Actions[SelectedIndex].Execute();
+    }
+}
