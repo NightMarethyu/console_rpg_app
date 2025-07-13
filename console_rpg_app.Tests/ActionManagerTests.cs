68fcf52b795cd46a39c4e6ba7cacb81900529bae
Ammon <44215158+NightMarethyu@users.noreply.github.com>
2025-07-12 16:51:49 -1000

    Test: Add multiple tests of ActionManager
    I've added several tests of the ActionManager to make sure that the correct number of actions are being returned under different circumstances

--- console_rpg_app.Tests/ActionManagerTests.cs
+++ console_rpg_app.Tests/ActionManagerTests.cs
@@ -42,5 +42,51 @@
 
             Assert.DoesNotContain(actions, action => action.Description.StartsWith("Attack"));
         }
+
+        [Fact]
+        public void GetCurrentActions_Should_ReturnMoveActions_ForEach_LocationExit()
+        {
+            var (mapManager, characterManager) = new TestWorldBuilder()
+                .WithExits(3)
+                .Build();
+
+            var actionManager = new ActionManager(mapManager, characterManager);
+
+            var actions = actionManager.GetCurrentActions();
+
+            var filteredActions = actions.Where(a => a.Description.StartsWith("Move")).ToList();
+
+            Assert.Equal(3, filteredActions.Count);
+        }
+
+        [Fact]
+        public void GetCurrentActions_Should_ReturnAttackAction_For_Each_LivingEnemyInLocation()
+        {
+            var (mapManager, characterManager) = new TestWorldBuilder()
+                .WithEnemies(3)
+                .WithEnemies(1, false)
+                .Build();
+
+            var actionManager = new ActionManager(mapManager, characterManager);
+
+            var actions = actionManager.GetCurrentActions();
+
+            var filteredActions = actions.Where(a => a.Description.StartsWith("Attack")).ToList();
+
+            Assert.Equal(3, filteredActions.Count);
+        }
+
+        [Fact]
+        public void GetCurrentActions_Should_CombineActions_From_Player_Location_And_Characters()
+        {
+            var (mapManager, characterManager) = new TestWorldBuilder()
+                .WithEnemies(3)
+                .WithExits(3)
+                .Build();
+
+            var actionManager = new ActionManager(mapManager, characterManager);
+
+            Assert.Equal(7, actionManager.GetCurrentActions().Count);
+        }
     }
 }
